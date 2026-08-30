c4c4dd8504f7aba467d2952218dbb15c3abacc2f
Geethakumari R <geetharatnam45@gmail.com>
2026-08-30 19:26:00 +0800

    Delete src directory

--- src/components/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
-
-export default function PrivateRoute({ children }) {
-  const { user } = useAuth();
-  return user ? children : <Navigate to="/login" replace />;
-}
